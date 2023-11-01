e9a613478f5e910c4d25b7e60b45a5c4c63fa7a4
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2023-11-01 22:14:34 +0545

    feat: Add responsive navigation bar component
    Created a responsive navigation bar component using React and styled it to adapt to different screen sizes. The logo size adjusts based on the screen width, and the navigation links are properly organized for both desktop and mobile devices. Implemented a menu icon for mobile view. This navbar enhances the user experience on various devices.

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.png';
+import './NavbarStyle.css';
+
+class Navbar extends Component {
+  state = { clicked: false }
+handleClick = () =>{
+  this.setState({clicked: ! this.state.clicked})
+}
+  render() {
+    return (
+      <>
+        <nav>
+          <img src={logo} className="logo" alt="Logo" />
+<h1> SHREE BUDDHA BHAWANI SECONDARY SCHOOL <br/>
+Khandadevi-6, Makadum
+  
+</h1>
+          <div>
+            <ul id="navbar"
+            className={this.state.clicked ? "#navbar active" :"#navbar"}
+            >
+              <li><a className="active" href="index.html">Home</a></li>
+              <li><a href="index.html">About Us</a></li>
+              <li><a href="index.html">Our Staff</a></li>
+              <li><a href="index.html">News</a></li>
+              <li><a href="index.html">Contact</a></li>
+            </ul>
+          </div>
+          <div id="mobile" onClick={this.handleClick}>
+          <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Navbar;
