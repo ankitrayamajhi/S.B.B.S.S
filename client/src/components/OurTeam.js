8ca767651f4ce97729d93dc162ee86642f146e7f
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2023-11-03 20:34:42 +0545

    feat: Add LinkedIn, Twitter, and Facebook icons to social links in OurTeam component

--- client/src/components/OurTeam.js
+++ client/src/components/OurTeam.js
@@ -3,34 +3,73 @@ import '../assets/css/OurTeamStyle.css';
 
 const staffData = [
   {
-    name: 'Vin Diesel',
-    role: 'Senior App Developer',
+    name: 'Ram Sharma',
+    role: 'Principal',
     image: 'https://1.bp.blogspot.com/-8c7QTLoyajs/YLjr2V6KYRI/AAAAAAAACO8/ViVPQpLWVM0jGh3RZhh-Ha1-1r3Oj62wQCNcBGAsYHQ/s16000/team-1-3.jpg',
-    socialLinks: [
-      'https://www.facebook.com/',
-      'https://www.instagram.com/',
-      'https://twitter.com/',
-    ],
+    socialLinks: {
+      facebook: 'https://www.facebook.com/',
+      instagram: 'https://www.instagram.com/',
+      twitter: 'https://twitter.com/',
+      linkedin: 'https://www.linkedin.com/',
+    },
   },
+ 
   {
-    name: 'David Corner',
-    role: 'Front End Developer',
-    image: 'https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAjQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg',
-    socialLinks: [
-      'https://www.facebook.com/',
-      'https://www.instagram.com/',
-      'https://twitter.com/',
-    ],
-  },
-  {
-    name: 'Tom Cruise',
-    role: 'Full Stack Developer',
+    name: 'Sita Rai',
+    role: 'Vice - Principal',
     image: 'https://1.bp.blogspot.com/-AO5j2Y9lzME/YLjr3mxiqAI/AAAAAAAACPE/KAaYYTtQTrgBE3diTbxGoc4U4fCGx-C2gCNcBGAsYHQ/s16000/team-1-4.jpg',
-    socialLinks: [
-      'https://www.facebook.com/',
-      'https://www.instagram.com/',
-      'https://twitter.com/',
-    ],
+    socialLinks: {
+      facebook: 'https://www.facebook.com/',
+      instagram: 'https://www.instagram.com/',
+      twitter: 'https://twitter.com/',
+      linkedin: 'https://www.linkedin.com/',
+    },
+  },
+  {
+    name: 'Ram Sharma',
+    role: 'Principal',
+    image: 'https://1.bp.blogspot.com/-8c7QTLoyajs/YLjr2V6KYRI/AAAAAAAACO8/ViVPQpLWVM0jGh3RZhh-Ha1-1r3Oj62wQCNcBGAsYHQ/s16000/team-1-3.jpg',
+    socialLinks: {
+      facebook: 'https://www.facebook.com/',
+      instagram: 'https://www.instagram.com/',
+      twitter: 'https://twitter.com/',
+      linkedin: 'https://www.linkedin.com/',
+    },
+  },
+ 
+  {
+    name: 'Sita Rai',
+    role: 'Vice - Principal',
+    image: 'https://1.bp.blogspot.com/-AO5j2Y9lzME/YLjr3mxiqAI/AAAAAAAACPE/KAaYYTtQTrgBE3diTbxGoc4U4fCGx-C2gCNcBGAsYHQ/s16000/team-1-4.jpg',
+    socialLinks: {
+      facebook: 'https://www.facebook.com/',
+      instagram: 'https://www.instagram.com/',
+      twitter: 'https://twitter.com/',
+      linkedin: 'https://www.linkedin.com/',
+    },
+  },
+  {
+    name: 'Ram Sharma',
+    role: 'Principal',
+    image: 'https://1.bp.blogspot.com/-8c7QTLoyajs/YLjr2V6KYRI/AAAAAAAACO8/ViVPQpLWVM0jGh3RZhh-Ha1-1r3Oj62wQCNcBGAsYHQ/s16000/team-1-3.jpg',
+    socialLinks: {
+      facebook: 'https://www.facebook.com/',
+      instagram: 'https://www.instagram.com/',
+      twitter: 'https://twitter.com/',
+      linkedin: 'https://www.linkedin.com/',
+    },
+  },
+ 
+  {
+    name: 'Sita Rai',
+    role: 'Vice - Principal',
+    image: 'https://1.bp.blogspot.com/-AO5j2Y9lzME/YLjr3mxiqAI/AAAAAAAACPE/KAaYYTtQTrgBE3diTbxGoc4U4fCGx-C2gCNcBGAsYHQ/s16000/team-1-4.jpg',
+    socialLinks: {
+      facebook: 'https://www.facebook.com/',
+      instagram: 'https://www.instagram.com/',
+      twitter: 'https://twitter.com/',
+      linkedin: 'https://www.linkedin.com/',
+    },
   },
 ];
 
@@ -54,9 +93,12 @@ const OurTeam = () => {
               <h3>{staff.name}</h3>
               <p>{staff.role}</p>
               <div className="social-links">
-                {staff.socialLinks.map((link, i) => (
-                  <a href={link} key={i} target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-facebook"></i>
+                {Object.keys(staff.socialLinks).map((linkType, i) => (
+                  <a href={staff.socialLinks[linkType]} key={i} target="_blank" rel="noopener noreferrer">
+                    {linkType === 'linkedin' && <i className="fab fa-linkedin"></i>}
+                    {linkType === 'twitter' && <i className="fab fa-twitter"></i>}
+                    {linkType === 'facebook' && <i className="fab fa-facebook"></i>}
+                    {linkType === 'instagram' && <i className="fab fa-instagram"></i>}
                   </a>
                 ))}
               </div>
