e3d1dce22f7e4a134d9d35abc701a801361dfdfe
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2023-11-29 21:25:40 +0545

    refactor(home): update functionality in Home.js
    This commit refactors the Home.js file, updating existing functionalities or adding new features to the Home component.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,12 +3,17 @@ import React from 'react';
 import Navbar from './Navbar';
 import OurTeam from './OurTeam';
 import Footer from './Footer';
+import About from './About';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <About/>
+      <br></br>
+    
 
+      <br></br>
       <OurTeam />
     
       <Footer />
