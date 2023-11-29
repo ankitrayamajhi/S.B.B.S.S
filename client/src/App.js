ade296031014c7dc771961c7b9030ec24ac61a96
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2023-11-29 21:27:42 +0545

    chore(app): update configuration or dependencies in App.js
    This commit updates the configuration settings, dependencies, or overall structure in the App.js file.

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OurTeam from './components/OurTeam';
 import Home from './components/Home';
 import Notice from './components/Notice';
-import ResultCheck from './components/ResultCheck';
-
+import Course from './components/Course';
 function App() {
   return (
     <Router>
@@ -14,10 +13,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/OurTeam" element={<OurTeam />} />
           <Route path="/Notice" element={<Notice />} />
-          <Route path="/Results" element={<ResultCheck />} />
+
 
         </Routes>
-        
       </div>
     </Router>
   );
