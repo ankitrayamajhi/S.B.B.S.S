f3452c98f492e842aa1dd77decdca49ec1408fb2
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2023-12-07 22:24:40 +0545

    feat(footer): Implement footer section
    Added a footer section across all pages to provide consistent navigation and information.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,122 +1,10 @@
 // import React from 'react';
 import '../assets/css/FooterStyle.css';
-import logo from '../assets/img/logo.png';
 const Footer = () => {
   return (
     <div>
-      <body>
+     
         <footer className="footer-section">
-          <div className="container">
-            <div className="footer-cta pt-5 pb-5">
-              <div className="row">
-                <div className="col-xl-4 col-md-4 mb-30">
-                  <div className="single-cta">
-                    <i className="fas fa-map-marker-alt" />
-                    <div className="cta-text">
-                      <h4>Find us</h4>
-                      <span>Khandadevi-6, Makadum, Ramechhap</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-md-4 mb-30">
-                  <div className="single-cta">
-                    <i className="fas fa-phone" />
-                    <div className="cta-text">
-                      <h4>Call us</h4>
-                      <span>9876543210 0</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-md-4 mb-30">
-                  <div className="single-cta">
-                    <i className="far fa-envelope-open" />
-                    <div className="cta-text">
-                      <h4>Mail us</h4>
-                      <span>mail@info.com</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="footer-content pt-5 pb-5">
-              <div className="row">
-                <div className="col-xl-4 col-lg-4 mb-50">
-                  <div className="footer-widget">
-                    <div className="footer-logo">
-                      <a href="index.html">
-                        <img src={logo} className="logo" alt="logo" />
-                      </a>
-                    </div>
-                    <div className="footer-text">
-                      <p>
-                        Lorem ipsum dolor sit amet, consec tetur adipisicing
-                        elit, sed do eiusmod tempor incididuntut consec tetur
-                        adipisicing elit, Lorem ipsum dolor sit amet.
-                      </p>
-                    </div>
-                    <div className="footer-social-icon">
-                      <span>Follow us</span>
-                      <a href="https://www.facebook.com/profile.php?id=100063768624765">
-                        <i className="fab fa-facebook-f facebook-bg" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-twitter twitter-bg" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-google-plus-g google-bg" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
-                  <div className="footer-widget">
-                    <div className="footer-widget-heading">
-                      <h3>Useful Links</h3>
-                    </div>
-                    <ul>
-                      <li>
-                        <a href="#">Home</a>
-                      </li>
-                      <li>
-                        <a href="#">About Us</a>
-                      </li>
-                      <li>
-                        <a href="#">Our Team</a>
-                      </li>
-                      <li>
-                        <a href="#">News</a>
-                      </li>
-                      <li>
-                        <a href="#">Contact</a>
-                      </li>
-                     
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
-                  <div className="footer-widget">
-                    <div className="footer-widget-heading">
-                      <h3>Subscribe</h3>
-                    </div>
-                    <div className="footer-text mb-25">
-                      <p>
-                        Don’t miss to subscribe to our new feeds, kindly fill
-                        the form below.
-                      </p>
-                    </div>
-                    <div className="subscribe-form">
-                      <form action="#">
-                        <input type="text" placeholder="Email Address" />
-                        <button>
-                          <i className="fab fa-telegram-plane" />
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
           <div className="copyright-area">
             <div className="container">
               <div className="row">
@@ -153,7 +41,7 @@ const Footer = () => {
             </div>
           </div>
         </footer>
-      </body>
+    
     </div>
   );
 };
