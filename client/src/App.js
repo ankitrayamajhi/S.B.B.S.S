64507fec8d5bc7aaa15ed7294577139ec3f20394
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2023-12-11 22:38:05 +0545

    chore(app): Update configurations and routes
    Adjusted configurations for better performance and updated routing logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import OurTeam from './components/OurTeam';
 import Home from './components/Home';
+import About from './components/About';
 import Notice from './components/Notice';
-import Course from './components/Course';
+import Footer from './components/Footer'
+import Contact from './components/Contact';
+import Login from './components/Admin/Login';
+
 function App() {
   return (
     <Router>
       <div className="App">
-      
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
           <Route path="/OurTeam" element={<OurTeam />} />
           <Route path="/Notice" element={<Notice />} />
-
-
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/admin/login" element={<Login />} />
         </Routes>
+      
+        <Footer />
       </div>
     </Router>
   );
